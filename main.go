50bbaba67ffe421d68c13d60bd76d840c07da05d
adaskalova <asia.grigorova@gmail.com>
2023-02-02 16:12:16 +0200

    Add isEmpty() and isNum() functions Add take user input Add comments

--- main.go
+++ main.go
@@ -1,12 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"path"
+	"strings"
+	"unicode"
 )
 
-func main() {
+var (
+	bFlag  bool
+	bDigit bool
+)
 
-	cmd := os.Args[0]
-	fmt.Printf("Program Name: %s\n", cmd)
+func isEmpty(str string) bool {
+	if strings.TrimSpace(str) == "" {
+		// if string is empty
+		bFlag = true
+	} else {
+		bFlag = false
+	}
+	return bFlag
+}
+
+func isNum(str string) bool {
+	for _, ch := range str {
+		if unicode.IsNumber(ch) {
+			fmt.Println(string(ch), "is a number.")
+			bDigit = true
+		} else {
+			//is not a number rune
+			bDigit = false
+		}
+	}
+	return bDigit
+}
+
+func main() {
+	var input string
+	//path to the program
+	name := os.Args[0]
+	//return the file name
+	name = path.Base(name)
+	//display the program name
+	fmt.Println("Program Name: " + name)
+	//read the input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Enter your command:")
+		//scans the input
+		scanner.Scan()
+		//get string typed in the standard input
+		input = scanner.Text()
+
+		outEmpty := isEmpty(input)
+		if outEmpty {
+			fmt.Println("The value you enter isn't valid! Please, enter a valid command!")
+		}
+
+		outDigit := isNum(input)
+		if outDigit {
+			fmt.Println("The value you enter isn't valid! Please, enter a valid command!")
+		}
+
+		if input == "Q" || input == "q" {
+			// break from the for loop
+			break
+		}
+	}
+
 }
