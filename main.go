10c1d66afa3ce49dbb7814a7e8f69c27fb15e274
adaskalova <asia.grigorova@gmail.com>
2023-02-07 16:38:07 +0200

    Add executeCmd() and readOutput() functions.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -15,6 +16,7 @@ import (
 const (
 	colPurple = "\033[35m"
 	colNone   = "\033[0m"
+	colRed    = "\033[0;31m"
 )
 
 var (
@@ -26,6 +28,9 @@ var (
 
 func changeColor(s string) string {
 	return colPurple + s + colNone
+}
+func changeColRed(s string) string {
+	return colRed + s
 }
 
 func isEmpty(str string) bool {
@@ -85,7 +90,56 @@ func changeDir(str string) {
 	}
 }
 
+func executeCmd(inputCmd string) error {
+
+	cmd := exec.Command("bash", "-c", inputCmd)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr)
+		return err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr)
+		return err
+	}
+
+	coloredTxt := changeColRed("Error: ")
+
+	go readOutput(stdout, "Output: ")
+	go readOutput(stderr, coloredTxt)
+
+	// start the command
+	err = cmd.Start()
+	if err != nil {
+		panic("Error: The command could not be executed!")
+	}
+
+	//waiting for command to finish
+	err = cmd.Wait()
+	log.Printf("Completed successfully...")
+
+	return nil
+}
+
+func readOutput(reader io.Reader, prefix string) {
+	rdr := bufio.NewReader(reader)
+	result := ""
+	bs := []byte{}
+	for bs != nil {
+		bs, _, _ = rdr.ReadLine()
+		result = string(bs)
+		coloredTxt := changeColor(result)
+		fmt.Println(prefix + coloredTxt)
+		if bs == nil {
+			break
+		}
+	}
+}
+
 func main() {
+
 	var input string
 	//path to the program
 	name := os.Args[0]
@@ -97,7 +151,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Print("Enter your command:")
+		fmt.Print("> Enter your command:")
 		//scans the input
 		scanner.Scan()
 		//get string typed in the standard input
@@ -116,23 +170,8 @@ func main() {
 
 			if bHasPrefix {
 				changeDir(input)
-			} else {
-				str := "The value you enter isn't valid! Please, enter a valid command!"
-				coloredTxt := changeColor(str)
-				fmt.Println(coloredTxt)
 			}
-
-			cmd := exec.Command(input)
-			out, err := cmd.Output()
-			if err != nil {
-				panic(err)
-			}
-			tmp := string(out)
-			tmp = changeColor(tmp)
-			fmt.Println("Output:" + string(tmp))
-
-			//TODO
-
+			executeCmd(input)
 		}
 
 		if input == "Q" || input == "q" {
