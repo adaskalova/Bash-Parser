019602f9d72fc83247ded4c02e16cdec2dbb8966
adaskalova <asia.grigorova@gmail.com>
2023-02-13 10:27:12 +0200

    Update cmd_sort directory. Update BenchmarkChangeDir function. Add sort test commands.

--- BashParser/main_test.go
+++ BashParser/main_test.go
@@ -5,7 +5,6 @@ import (
 )
 
 var path = "commands/test_files/test/rm_file.txt"
-var changePath = "cd /tmp"
 
 func BenchmarkIsPathExists(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -14,7 +13,19 @@ func BenchmarkIsPathExists(b *testing.B) {
 }
 
 func BenchmarkChangeDir(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		changeDir(changePath)
+	benchmarks := []struct {
+		name       string
+		changePath string
+	}{
+		{"Path_root:", "/"},
+		{"Path_tmp:", "/tmp"},
+		//{"Path_random:", "<path>/<to>/<the>/<specific>/<directory>"},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				changeDir(bm.changePath)
+			}
+		})
 	}
 }
